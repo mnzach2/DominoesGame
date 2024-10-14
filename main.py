0d7d4192b50a1346ce9b25fa92ea3a9c0d121bef
mnzach2 <138807640+mnzach2@users.noreply.github.com>
2024-10-14 12:36:53 -0500

    Update main.py
    Notes of to dos in general

---
+++ main.py
@@ -0,0 +1,8 @@
+# TO DO:
+# Create 2 players from CPlayer
+# Have them draw 7 tiles each from CRandom
+# Pick a tile as the starter
+# Players play tiles if they can connect them, or draw from boneyard until they can
+# Winner is whoever is out of tiles first
+# Tie if neither player can play and boneyard is empty
+# Keep track of what is playable where in CDominos
